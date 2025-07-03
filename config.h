90f4f18e7db1ad50b0364dd3921c231bd4b645fa
krishna <krishna404@yandex.com>
2025-07-03 17:21:36 +0530

    update borderpx

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "Aporetic Sans Mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 20;
 
 /*
  * What program is execed by st depends of these precedence rules:
